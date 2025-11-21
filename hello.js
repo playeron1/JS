82d8284ac8016dabacf897017206551d0e4cc0aa
playeron1 <krishnshah2003@gmail.com>
2025-11-21 23:46:25 +0530

    Resumed coding in hello.js

---
+++ hello.js
@@ -0,0 +1,4 @@
+console.log("Hello World !!!")
+console.log("Did this file @ 11:44 PM for testing laptop with HDMI cable.");
+console.log("Found it very cool. Typing this without looking at the keyboard and looking at the monitr wirh some spwllinf misyaks 😊");
+
